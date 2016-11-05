a3deea459e89d1da154a366e8a58bd4663a9c552
OliveSerg <oliveserg@gmail.com>
2016-11-05 23:12:01 +0000

    server running

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
-import express from 'express';
+const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 
 app.use('/src', express.static(__dirname + "/src"));
+
+http.listen(8080, function(){
+	console.log('listening on *:8080');
+});
 
 app.get('/getImages', function(req, res){
     console.log('hit')
