58083bb46de07715fb754ac6c676ca2758c9a219
OliveSerg <oliveserg@gmail.com>
2016-11-07 11:21:58 -0500

    masonry and image styling

--- server.js
+++ server.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
+const fs = require('fs');
 
 app.use('/src', express.static(__dirname + "/src"));
 
-http.listen(8080, function(){
-	console.log('listening on *:8080');
+http.listen(3000, function(){
+	console.log('listening on *:3000');
 });
 
-app.get('/getImages', function(req, res){
-    console.log('hit')
+app.all('*', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token');
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+})
+                              
+app.get('/images', function(req, res){
+    const folder = req.query.folder
+    var files = fs.readdirSync(`./src/images/${folder}/`)
+    files = JSON.stringify(files)
+    res.send(files)
 })
--- src/pages/GraphicIllustrations.js
+++ src/pages/GraphicIllustrations.js
@@ -5,44 +5,44 @@ export default class GraphicIllustrations extends React.Component {
     constructor(props){
         super()
         this.state = {
-            illustrations: false,
-            graphicDesigns: false
+            illustrations: ["No Images"],
+            graphicDesigns: ["No Images"], 
         }
     }
 
     componentDidMount(){
         const {getImages} = this.props
-        const requests = [getImages('Digital'), getImages('Graphic')]
+        const folder = ['Digital', 'Graphic']
+        const requests = [getImages(folder[0]), getImages(folder[1])]
         Promise.all(requests).then((responses)=>{
-                console.log(responses)
-                this.setState({
-                    illustrations: responses[0].data,
-                    graphicDesigns: responses[1].data
-                })
-             }).catch((error)=>{
-                 return "No files";
-             })
+            this.setState({
+                illustrations: this.makeImgComp( folder[0], responses[0].data),
+                graphicDesigns: this.makeImgComp( folder[1], responses[1].data)
+            })
+        }).catch((error)=>{
+            return "No files";
+        })
+    }
+    
+    makeImgComp(folder, array){
+        return array.map((image)=>{
+            var source = `../images/${folder}/${image}`
+            return <div className='gallery-item'><img src={source}/></div>
+        })
     }
 
     render(){
         const {illustrations, graphicDesigns} = this.state
-        let gallery = ["No Images"]
-        if(false){
-            gallery = images.map((image)=>{
-                var source =`../images/Digital/${image}` 
-                return <img src={source}/>
-            })
-        }
         return(
             <div>
                 <img className="title-image" src="../images/img-2.jpg"/>
                 <h2>Digital Illustrations</h2>
-                <div>
-                    {gallery}
+                <div className='gallery'>
+                    {illustrations}
                 </div>
                 <h2>Graphic Design</h2>
-                <div>
-                    {gallery}
+                <div className='gallery'>
+                    {graphicDesigns}
                 </div>
             </div>        
         ) 
