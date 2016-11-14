4f0d593d2cdcc5d2c4ded673eaf1d5f8348a72e2
OliveSerg <oliveserg@gmail.com>
2016-11-13 21:06:23 -0500

    3d models added and model loader

--- src/images/images.js
+++ src/images/images.js
@@ -160,7 +160,124 @@ modeling:{
         {
             name: 'LemmyKart',
             source: 'https://p3d.in/e/QOejF',
+            image: 'https://p3d.in/static/uploads/43586/image-1eb8533649.png',
             caption: "Lemmy!!!"
+        },
+        {
+            name: 'Wheel Chair',
+            source:'https://p3d.in/e/uLWBR',
+            image:'https://p3d.in/static/uploads/43586/image-2aa6535783.png',
+            caption: "Wheel chair"
+        },{
+            name: 'Garden Urn Normals',
+            source:'https://p3d.in/e/zosVW',
+            image:'https://p3d.in/static/uploads/43586/image-915f533597.png',
+            caption: "Vase"
+        },{
+            name: 'Demon Bane and Shield of Diablo',
+            source:'https://p3d.in/e/eYPuL',
+            image:'https://p3d.in/static/uploads/43586/image-444f561348.png',
+            caption:"DOOM"
+        },{
+            name: 'Beast Bane and Shield of Dismay',
+            source:'https://p3d.in/e/8TASL',
+            image:'https://p3d.in/static/uploads/43586/image-33ac533577.png',
+            caption:"SAD"
+        },{
+            name: 'Modular Barn',
+            source: 'https://p3d.in/e/tta7w',
+            image:'https://p3d.in/static/uploads/43586/image-be1c533585.png',
+            caption:"NEIGH"
+        },{
+            name: 'Barrel Trough',
+            source: 'https://p3d.in/e/6fNKa',
+            image:'https://p3d.in/static/uploads/43586/image-b96d533589.png',
+            caption:"Rollling"
+        },{
+            name: 'Sewage',
+            source:'https://p3d.in/e/R4CiD',
+            image:'https://p3d.in/static/uploads/43586/image-9088535801.png',
+            caption:"Ew"
+        },{
+            name: 'Papers',
+            source:'https://p3d.in/e/SCMHf',
+            image:'https://p3d.in/static/uploads/43586/image-f470535779.png',
+            caption:"ROLLING"
+        },{
+            name: 'Lights',
+            source:'https://p3d.in/e/C0S39',
+            image:'https://p3d.in/static/uploads/43586/image-d6cb535777.png',
+            caption: "All of the Lights!"
+        },{
+            name: 'Garbage Bag',
+            source: 'http://p3d.in/e/zadA2',
+            image: 'https://p3d.in/static/uploads/43586/image-d5ba535773.png',
+            captions:"Garbadore"
+        },{
+            name: 'Dumpster',
+            source: 'http://p3d.in/e/fIznO',
+            image: 'https://p3d.in/static/uploads/43586/image-b8d8535771.png',
+            captions: "Garbadores Home"
+        },{
+            name: 'Crate',
+            source: 'http://p3d.in/e/TK3VM',
+            image: 'https://p3d.in/static/uploads/43586/image-d4a7535769.png',
+            captions: 'Somethings inside'
+        },{
+            name: 'Cans',
+            source: 'http://p3d.in/e/ZptWK',
+            image: 'https://p3d.in/static/uploads/43586/image-ec45535765.png',
+            captions: "Product placement"
+        },{
+            name: 'Boxes',
+            source: 'http://p3d.in/e/87Nao',
+            image: 'https://p3d.in/static/uploads/43586/image-bab6535767.png',
+            captions: "Hobilo homes"
+        },{
+            name: 'Barrels',
+            source: 'http://p3d.in/e/08pH2',
+            image: 'https://p3d.in/static/uploads/43586/image-c0d9535759.png',
+            captions:"BARRELS!!"
+        },{
+            name: 'Little Red Punk',
+            source: 'http://p3d.in/e/ZDmGB',
+            image: 'https://p3d.in/static/uploads/43586/image-3b69533581.png',
+            captions: "Avril Lavigne"
+        },{
+            name: 'Hallway',
+            source: 'http://p3d.in/e/U3p1A',
+            image: 'https://p3d.in/static/uploads/43586/image-970e535815.png',
+            captions: "Boo"
+        },{
+            name: 'Buhl Gragos',
+            source: 'http://p3d.in/e/ffXpm',
+            image: 'https://p3d.in/static/uploads/43586/image-e5c7535845.png',
+            captions: "Piggy Piggy"
+        },{
+            name: 'Environment Dumpster',
+            source: 'http://p3d.in/e/KBuMO',
+            image: 'https://p3d.in/static/uploads/43586/image-071b435704.png',
+            captions: "Cabbage dump"
+        },{
+            name: 'Little Red',
+            source: 'http://p3d.in/e/w90fY',
+            image: 'https://p3d.in/static/uploads/43586/image-4d78433880.png',
+            captions: "Chroma Avril Lavigne"
+        },{
+            name: 'Shield of Dismay',
+            source: 'http://p3d.in/e/6DYPd',
+            image: 'https://p3d.in/static/uploads/43586/image-5638429233.png',
+            captions: 'SAD shield'
+        },{
+            name: 'Beast Bane',
+            source: 'http://p3d.in/e/2EJKc',
+            image: 'https://p3d.in/static/uploads/43586/image-afc3427009.png',
+            captions:"Pointy"
+        },{
+            name: 'Nut Crunch Ceral',
+            source: 'http://p3d.in/e/BO4vN',
+            image: 'https://p3d.in/static/uploads/43586/image-973a418181.png',
+            captions: "Cpt Crunch"
         },
     ]}
 }
--- src/pages/3DModeling.js
+++ src/pages/3DModeling.js
@@ -10,17 +10,39 @@ export default class Modeling extends React.Component {
         }
     }
 
-    makeImgComp(array, folder){
-        return array.map((image, index)=>{
-            var source = image.source
-            return <div className='gallery-item'><img id={folder + index} src={source}/><p className='caption hidden'>{image.caption}</p></div>
+    makeImgComp(source, caption, id, fc){
+        return <div className='gallery-item'><img onClick={fc} id={id} src={source}/><p className='caption hidden'>{caption}</p></div>
+    }
+
+    toggleModel(ev){
+        this.setState({
+            current: ev.target.id
         })
     }
 
     render(){
-        const {images, id} = this.state
+        const {images, id, current} = this.state
+        const imgComps = images.map((img, index)=>{
+            return this.makeImgComp(img.image, img.caption,"m"+index, this.toggleModel.bind(this))
+        })
+        let modelComp = ""
+        if(current && current.match(/^m/)){
+            const model = current.match(/\d+/)[0]
+            modelComp = <div className="model-box"><iframe allowfullscreen webkitallowfullscreen width="640" height="480" frameborder="0" seamless src={images[model].source}></iframe></div>
+        }
+        const titleImgStyle = {
+            background: "url('../images/img-4.jpg') no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center"
+        }
         return (
-                <iframe allowfullscreen webkitallowfullscreen width="640" height="480" frameborder="0" seamless src={images[0].source}></iframe>
+                <div>
+                    <div className="title-image" style={titleImgStyle}><input type='button' className='page-down'/></div>
+                    <div className="gallery">
+                        {modelComp} 
+                        {imgComps}
+                    </div>
+                </div>
     );
                 
     }
