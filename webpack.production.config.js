8674275427a83df4833f6f414a1fdd4de4bfc980
OliveSerg <oliveserg@gmail.com>
2016-11-18 17:29:10 -0500

    works

---
+++ webpack.production.config.js
@@ -0,0 +1,53 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports ={
+    context: path.join(__dirname, 'src'),
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.html$/,
+                loader: "file?name=[name].[ext]",
+            },
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.(png|jpg|ttf|woff|svg|otf|eot|svg).*?$/,
+                loader: "file-loader"
+            },
+        ]
+    },
+    plugins: [ 
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.AggressiveMergingPlugin()
+            ],
+    resolve: {
+        root: [path.resolve('./src/')],
+        extensions: ['', '.js', '.css']
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        historyApiFallBack: true
+    }
+}
